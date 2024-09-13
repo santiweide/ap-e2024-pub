0150ea3c72c205585c49381779eb30a88bf26cbc
santiweide <ggy_778@outlook.com>
2024-09-13 14:29:02 +0200

    Finished a2-task1

--- a2/a2-handout/src/APL/Eval.hs
+++ a2/a2-handout/src/APL/Eval.hs
@@ -28,40 +28,68 @@ envLookup v env = lookup v env
 
 type Error = String
 
-newtype EvalM a = EvalM (Env -> Either Error a)
+
+-- [String] is user controlled String
+newtype EvalM a = EvalM (Env -> ([String], Either Error a))
+-- question1 what if 
+--      newtype EvalM a String = EvalM (Env -> ([String], Either Error a))
 
 instance Functor EvalM where
-  fmap = liftM
+  -- fmap :: (a -> b) -> EvalM a -> EvalM b
+  fmap f (EvalM t) = EvalM $ \env ->
+        let (log, x) = t env
+        in case x of
+            Left err -> (log, Left err)
+            Right y -> (log, Right (f y))
 
+            
 instance Applicative EvalM where
-  pure x = EvalM $ \_env -> Right x
-  (<*>) = ap
+  pure x = EvalM $ \_env -> ([], Right x)
+  
+  -- <*> :: EvalM (a -> b) -> EvalM a -> EvalM b
+  (EvalM f) <*> (EvalM v) = EvalM $ \env ->
+    let (log1, ff) = f env
+        (log2, vv) = v env
+    in case ff of
+         Left err   -> (log1 ++ log2, Left err)
+         Right func -> case vv of
+                         Left err -> (log1 ++ log2, Left err)
+                         Right val -> (log1 ++ log2, Right (func val))
+
 
 instance Monad EvalM where
+  -- >>= :: EvalM a -> (a -> EvalM b) -> EvalM b
   EvalM x >>= f = EvalM $ \env ->
-    case x env of
-      Left err -> Left err
-      Right x' ->
-        let EvalM y = f x'
-         in y env
+    let (log1, xx) = x env
+    in case xx of
+         Left err -> (log1, Left err)
+         Right val ->
+           let EvalM f' = f val
+               (log2, y) = f' env
+           in (log1 ++ log2, y)
+
+
+-- The function evalPrint adds a string to the list of printed strings. Use this to implement the eval case for Print.
 
 askEnv :: EvalM Env
-askEnv = EvalM $ \env -> Right env
+askEnv = EvalM $ \env -> ([], Right env)
 
 localEnv :: (Env -> Env) -> EvalM a -> EvalM a
 localEnv f (EvalM m) = EvalM $ \env -> m (f env)
 
+-- question: how to have the current [String] in the EvalM a?
+-- or we have just no need for that
 failure :: String -> EvalM a
-failure s = EvalM $ \_env -> Left s
+failure s = EvalM $ \_env -> ([], Left s)
 
 catch :: EvalM a -> EvalM a -> EvalM a
 catch (EvalM m1) (EvalM m2) = EvalM $ \env ->
   case m1 env of
-    Left _ -> m2 env
-    Right x -> Right x
+    (log, Left _) -> m2 env
+    (log, Right x) -> (log, Right x)
 
 runEval :: EvalM a -> ([String], Either Error a)
-runEval (EvalM m) = ([], m envEmpty)
+runEval (EvalM m) = m envEmpty
 
 evalIntBinOp :: (Integer -> Integer -> EvalM Integer) -> Exp -> Exp -> EvalM Val
 evalIntBinOp f e1 e2 = do
@@ -77,7 +105,21 @@ evalIntBinOp' f e1 e2 =
   where
     f' x y = pure $ f x y
 
+evalPrint :: String -> EvalM ()
+evalPrint msg = EvalM $ \env -> ([msg], Right ())
+
+showVal :: Val -> String
+showVal val = case val of
+    (ValInt x) -> show x
+    (ValBool x) -> show x
+    (ValFun env varname x) -> "#<func>"
+
 eval :: Exp -> EvalM Val
+eval (Print str exp) = do
+    val <- eval exp
+    evalPrint (str ++ ": " ++ (showVal val))
+    pure $ val
+            
 eval (CstInt x) = pure $ ValInt x
 eval (CstBool b) = pure $ ValBool b
 eval (Var v) = do
@@ -127,4 +169,6 @@ eval (Apply e1 e2) = do
       failure "Cannot apply non-function"
 eval (TryCatch e1 e2) =
   eval e1 `catch` eval e2
+
+
 
