a2cc3daa81ca7500cd72a1473b1f8d2ac768f657
santiweide <ggy_778@outlook.com>
2024-09-19 15:20:25 +0200

    fix

--- a3/a3-handout/src/APL/Parser_Tests.hs
+++ a3/a3-handout/src/APL/Parser_Tests.hs
@@ -72,5 +72,21 @@ tests =
         [ parserTest "x y z" $ (Apply (Apply (Var "x") (Var "y")) (Var "z")),
           parserTest "x(y z)" $ (Apply (Var "x") (Apply (Var "y") (Var "z"))),
           parserTestFail "x if x then y else z"
+        ],
+      testGroup
+        "Print, get and put"
+        [ parserTest "put x y" $ (KvPut (Var "x") (Var "y")),
+          parserTest "print \"foo\" x" $ (Print "foo" (Var "x")),
+          parserTest "get x" $ (KvGet (Var "x")),
+          parserTest "getx" $ (Var "getx"),
+          parserTestFail "get x y" -- special case for get is priorer than apply
+        ],
+      testGroup
+        "Let, try-catch and lambda"
+        [ parserTest "let x = y in z" $ (Let "x" (Var "y") (Var "z")),
+          parserTestFail "let true = y in z",
+          parserTestFail "x let v = 2 in v",
+          parserTest "try x+1 catch y+2" $ (TryCatch (Add (Var "x") (CstInt 1)) (Add (Var "y") (CstInt 2))),
+          parserTest "\\x -> x**x*y " $ (Lambda "x" (Mul (Pow (Var "x") (Var "x")) (Var "y")))
         ]
     ]
