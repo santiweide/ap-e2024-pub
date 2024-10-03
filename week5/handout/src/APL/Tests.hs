8394adebdc0b856bc24f0f7656e000d71a4db072
santiweide <ggy_778@outlook.com>
2024-10-03 02:13:07 +0200

    orz

--- week5/handout/src/APL/Tests.hs
+++ week5/handout/src/APL/Tests.hs
@@ -4,7 +4,10 @@ import APL.AST (Exp (..), VName)
 import Test.QuickCheck (Gen)
 
 genVar :: Gen VName
-genVar = undefined
+genVar = do
+    alpha <- elements ['a' .. 'z']
+    alphaNums <- listOf $ elements $ ['a' .. 'z'] ++ ['0' .. '9' ]
+    pure (alpha : alphaNums)
 
 genExp :: Gen Exp
 genExp = undefined
